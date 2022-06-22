5af2b3b7b11279350cd698bbaba7b61185adeaa4
itdvminh <dvminh.it@gmail.com>
2022-06-22 10:12:26 +0700

    MVC Router

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,12 @@
+
+class NewsController {
+    index(req, res) {
+        res.render('news')
+    }
+
+    detail(req, res) {
+        res.send('Detail')
+    }
+}
+
+module.exports = new NewsController
