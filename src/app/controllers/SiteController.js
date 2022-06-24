1616e05ef616de01583d7742b70fa87c3f26e694
itdvminh <dvminh.it@gmail.com>
2022-06-24 20:35:32 +0700

    Page Home display database

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,6 +1,15 @@
+const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
+
 class SiteController {
-    index(req, res) {
-        res.render('home');
+    index(req, res, next) {
+        Course.find({})
+            .then((Course) => {
+                res.render('home', {
+                    Course: mutipleMongooseToObject(Course),
+                });
+            })
+            .catch(next);
     }
 
     search(req, res) {
--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+
+// Connect to DB
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,13 +20,13 @@ app.use(express.json());
 //app.use(morgan('combined'))
 
 // Template engine
-    app.engine('hbs', engine({ extname: 'hbs' }));
-    app.set('view engine', 'hbs');
-    app.set('views', './src/resources/views');
-
+app.engine('hbs', engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`);
+    console.log(`App listening on http://localhost:${port}`);
+
     console.log(`-------------------------------------`);
 });
